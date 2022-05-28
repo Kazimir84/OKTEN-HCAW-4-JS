7c46875378d59a89092e4502bf72d8a750d28ef1
Kazimir84 <kav2007@ukr.net>
2022-05-28 14:51:29 +0300

    Add Addition Work!

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ class Client {
         this.email = email;
         this.phone = phone;
         this.order = order;
-    }
+    };
 };
 
 let client1 = new Client(3, 'Alex', 'Kazimir', 'kav29041984@gmail.com', 380672222, ['milk', 'bread', 'butter']);
@@ -400,60 +400,72 @@ console.log('Filterd ID ', filterIdPar);
 //======================================================================================================================
 // - Створити клас або функцію конструктор, за допомоги якої можна створювати об'єкти наступного вигляду.
 // Конструктор повинен приймати значення для кожної властивості, в т.ч і для властивостей внутрішніх об'єктів
+class Userr {
+    constructor(id, name, username, email, street, suite, city, zipcode, lat, lng, phone, website, companyName, catchPhrase, bs) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.address = {
+            street: street,
+            suite: suite,
+            city: city,
+            zipcode: zipcode,
+            geo: {
+                lat: lat,
+                lng: lng
+            }
+        };
+        this.phone = phone;
+        this.website = website;
+        this.company = {
+            name: companyName,
+            catchPhrase: catchPhrase,
+            bs: bs
+
+        };
+    };
+}
+let userr = new Userr(
+                    1,
+                    'Leanne Graham',
+                    'Bret',
+                    'Sincere@april.biz',
+                    'Kulas Light',
+                    'Apt. 556',
+                    'Gwenborough',
+                    '92998-3874',
+                    '-37.3159',
+                    '81.1496',
+                    '1-770-736-8031 x56442',
+                    'hildegard.org',
+                    'Romaguera-Crona',
+                    'Multi-layered client-server neural-net',
+                    'harness real-time e-markets'
+
+);
+console.log('User ', userr);
 
 // {
 //     id: 1,
-//         name: 'Leanne Graham',
+//     name: 'Leanne Graham',
 //     username: 'Bret',
 //     email: 'Sincere@april.biz',
 //     address: {
-//     street: 'Kulas Light',
+//          street: 'Kulas Light',
 //         suite: 'Apt. 556',
 //         city: 'Gwenborough',
 //         zipcode: '92998-3874',
-//         geo: {
-//         lat: '-37.3159',
-//             lng: '81.1496'
-//     }
-// },
+//              geo: {
+//                  lat: '-37.3159',
+//                  lng: '81.1496'
+//               }
+//      },
 //     phone: '1-770-736-8031 x56442',
-//         website: 'hildegard.org',
+//     website: 'hildegard.org',
 //     company: {
-//     name: 'Romaguera-Crona',
+//          name: 'Romaguera-Crona',
 //         catchPhrase: 'Multi-layered client-server neural-net',
 //         bs: 'harness real-time e-markets'
-// }
-// }
-
-
-// -  Створити функцію конструктор / клас  який описує об'єкт тегу
-// Поля :
-//     -назва тегу ()
-// - опис його дій
-// - масив з атрибутами (2-3 атрибути максимум)
-// Кожен атрибут описати як окремий який буде містити
-// -назву атрибуту
-// -опис дії атрибуту
-// інформацію брати з htmlbook.ru
-//
-// Таким чином описати теги
-// -a
-// -div
-// -h1
-// -span
-// -input
-// -form
-// -option
-// -select
-// Приклад результуючого об'єкту
-// {
-//     titleOfTag: 'area',
-//         action: `Каждый элемент <area> определяет активные области изображения, которые являются ссылками...`,
-//     attrs: [
-//     {titleOfAttr: 'accesskey', actionOfAttr: 'Переход к области с помощью комбинации клавиш'},
-//     {/*some props and values*/},
-//     {/*...*/},
-//     {/*...*/},
-// ]
-//
+//      }
 // }
